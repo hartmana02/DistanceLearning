5811c539191890acfc6748c3e121550fb640e24c
Andrew Hartmann <hartmana02@student-hartmana02.psdr3.org>
2020-05-06 14:52:58 -0500

    fixed

--- covid19_by_country.py
+++ covid19_by_country.py
@@ -11,7 +11,7 @@ df = pd.read_csv("covid19_data.csv", header=0)
 unique_countries = df["Country"].unique()
 
 for c in unique_countries:
-    if c in unique_countries: 
+    if c in countries: 
         date = df[df["Country"] == c]["Date"]
 
         confirmed_cases = df[df["Country"] == c]["Confirmed"]
@@ -19,7 +19,7 @@ for c in unique_countries:
         plt.plot(days, confirmed_cases, label=c, linestyle="-")
 
 plt.ylabel("Confirmed Cases (Logarithmic Scale)")
-#plt.yscale("log")
+plt.yscale("log")
 plt.xlabel("Days Since January 22nd, 2020")
 plt.title("Confirmed Cases of Covid-19 by Country")
 plt.legend()
