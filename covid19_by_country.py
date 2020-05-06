34c023188c0435f67629f1acaf2f9329dc56664b
Andrew Hartmann <hartmana02@student-hartmana02.psdr3.org>
2020-05-06 14:52:21 -0500

    logarithmic graph of convid-19 cases by country

---
+++ covid19_by_country.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+countries = ["US", "Spain", "Italy", "United Kingdom", "France", "Russia", "Iceland", "China", "India", "Korea, South"]
+days = []
+for x in range(104):
+    days.append(x)
+
+df = pd.read_csv("covid19_data.csv", header=0)
+
+unique_countries = df["Country"].unique()
+
+for c in unique_countries:
+    if c in unique_countries: 
+        date = df[df["Country"] == c]["Date"]
+
+        confirmed_cases = df[df["Country"] == c]["Confirmed"]
+
+        plt.plot(days, confirmed_cases, label=c, linestyle="-")
+
+plt.ylabel("Confirmed Cases (Logarithmic Scale)")
+#plt.yscale("log")
+plt.xlabel("Days Since January 22nd, 2020")
+plt.title("Confirmed Cases of Covid-19 by Country")
+plt.legend()
+plt.show()
